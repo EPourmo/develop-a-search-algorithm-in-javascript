8eefb3217f5d7ec6bce5ba3b67e84875364e68d7
EPourmo <94918200+EPourmo@users.noreply.github.com>
2022-06-10 21:37:21 +0200

    change createElement in template litteral

--- scripts/templates/RecipeCard.js
+++ scripts/templates/RecipeCard.js
@@ -12,33 +12,26 @@ export default class RecipeCard {
     const recipeCardBody = document.createElement("div");
     recipeCardBody.classList.add("recipe_card-body");
 
-    const recipeCardBodyHeader = document.createElement("div");
-    recipeCardBodyHeader.classList.add("recipe_card-body-header");
+    recipeCardBody.innerHTML = `
+    <div class="recipe_card-body-header">
+      <h2 class="recipe_card-title">${this._name}</h2>
+      <div class="recipe_card-time">
+        <p>${this._time} min</p>
+        <i class="fa-regular fa-clock"></i>
+      </div>
+    </div>`;
+
     const recipeCardBodyContent = document.createElement("div");
     recipeCardBodyContent.classList.add("recipe_card-body-content");
-
-    const recipeCardTitle = document.createElement("h2");
-    recipeCardTitle.classList.add("recipe_card-title");
-    recipeCardTitle.textContent = this._name;
-    const recipeCardTime = document.createElement("div");
-    recipeCardTime.classList.add("recipe_card-time");
-    const recipeCardTimeParagraph = document.createElement("p");
-    recipeCardTimeParagraph.textContent = `${this._time} min`;
-    const recipeCardTimeIcon = document.createElement("i");
-    recipeCardTimeIcon.setAttribute("class", "fa-regular fa-clock");
     const recipeCardIngredients = document.createElement("div");
     recipeCardIngredients.classList.add("recipe_card-ingredients");
     const recipeCardDescription = document.createElement("div");
     recipeCardDescription.classList.add("recipe_card-description");
-    const recipeCardDescriptionParagraph = document.createElement("p");
-    recipeCardDescriptionParagraph.textContent = this._description;
+    recipeCardDescription.innerHTML = `<p>${this._description}</p>`;
 
     recipeCard.append(recipeCardBody);
-    recipeCardBody.append(recipeCardBodyHeader, recipeCardBodyContent);
-    recipeCardDescription.append(recipeCardDescriptionParagraph);
-    recipeCardBodyHeader.append(recipeCardTitle, recipeCardTime);
+    recipeCardBody.append(recipeCardBodyContent);
     recipeCardBodyContent.append(recipeCardIngredients, recipeCardDescription);
-    recipeCardTime.append(recipeCardTimeParagraph, recipeCardTimeIcon);
 
     this._ingredients.forEach((ingredient) => {
       const recipeIngredientParagraph = document.createElement("p");
